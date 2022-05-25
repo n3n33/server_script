229f2771579a33294577b5b9818931db2ae64448
whi0331 <whi0331@hanmail.net>
2022-05-25 13:09:55 +0900

    path setup

---
+++ path_setting.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+files_name=$1
+
+createPath() {
+cat >>/etc/profile.d/${files_name} <<EOF
+
+export TEST_HOME=/home
+
+PATH=$PATH:$TEST_HOME
+
+EOF
+}
+
+createPath
+
+chmod +x /etc/profile.d/${files_name}
