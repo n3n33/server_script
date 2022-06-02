948a932151c5ddf0d1e144bf4a959403230dcb22
whi0331 <whi0331@hanmail.net>
2022-06-02 17:50:21 +0900

    log info add

--- python-excel/excel_macro.py
+++ python-excel/excel_macro.py
@@ -1,5 +1,8 @@
 from openpyxl import Workbook, workbook
 
+import logging
+import sys
+
 def create_workbook():
   # workbook 생성
   wb = Workbook()
@@ -7,9 +10,11 @@ def create_workbook():
   ws = wb.active
   # 시트명 변경
   ws.title = 'testsheet'
+  logging.info('Workbook sheet renamed')
   # 엑셀 저장
   wb.save('C:/Users/User/Desktop/excel_create_macro.xlsx')
+  logging.info('Excel stored')
   wb.close()
 
 
-create_workbook()
+# create_workbook()
