186ddd01b84c399df56d23777644b023f847c0f1
whi0331 <whi0331@hanmail.net>
2022-06-08 10:53:17 +0900

    ui

--- 2. json-macro/ui_sample.py
+++ 2. json-macro/ui_sample.py
@@ -1,5 +1,15 @@
-from curses import window
-import tkinker
+from tkinter import *
 
-window = tkinker.Tk()
-window.mainloop()
+root = Tk()
+
+lbl = Label(root, text="이름")
+lbl.pack()
+
+txt = Entry(root)
+txt.pack()
+
+btn = Button(root, text="OK")
+btn.pack()
+
+root.mainloop()
+
